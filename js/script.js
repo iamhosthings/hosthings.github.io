067cecc3e6021208ddda4f83fe71354900bf2b38
laurent bens <laurent22.b@gmail.com>
2023-02-19 21:38:07 +0100

    modify web enterprise

--- js/script.js
+++ js/script.js
@@ -1,12 +1,26 @@
 // When the user scrolls down 80px from the top of the document, resize the navbar's padding and the logo's font size
-window.onscroll = function() {scrollFunction()};
+(function () {
 
-function scrollFunction() {
-  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
-    document.getElementById("navbar").style.padding = "30px 10px";
-    document.getElementById("logo").style.fontSize = "25px";
-  } else {
-    document.getElementById("navbar").style.padding = "80px 10px";
-    document.getElementById("logo").style.fontSize = "35px";
-  }
-} 
+  var s = document.getElementById('img');
+  s.style.backgroundImage = "url('img/chatmain3.jpg')"
+  var timerID;
+  var compteur = 0;
+
+  setTimeout(function changeImg() {
+
+    if (s.style.backgroundImage === 'url("img/chatmain3.jpg")') {
+      s.style.backgroundImage = "url('img/chatmain2.jpg')"
+      compteur++;
+    } else {
+      s.style.backgroundImage = 'url("img/chatmain3.jpg")';
+      compteur++;
+    }
+    console.log('compteur', compteur)
+
+  }, 1000);
+
+  ;
+
+
+
+})();
