644c33e0c4f4fb591ccf1982e44a913b75633ebb
laurent bens <laurent22.b@gmail.com>
2023-02-20 09:49:46 +0100

    modify web enterprise

--- js/script.js
+++ js/script.js
@@ -3,24 +3,15 @@
 
   var s = document.getElementById('img');
   s.style.backgroundImage = "url('img/chatmain3.jpg')"
-  var timerID;
-  var compteur = 0;
+
 
   setTimeout(function changeImg() {
 
     if (s.style.backgroundImage === 'url("img/chatmain3.jpg")') {
       s.style.backgroundImage = "url('img/chatmain2.jpg')"
-      compteur++;
     } else {
       s.style.backgroundImage = 'url("img/chatmain3.jpg")';
-      compteur++;
     }
-    console.log('compteur', compteur)
 
-  }, 1000);
-
-  ;
-
-
-
+  }, 3000);
 })();
